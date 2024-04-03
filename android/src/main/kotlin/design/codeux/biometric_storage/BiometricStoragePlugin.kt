c77c263a0d4249b951b5d1ab3733d885ac971241
SuhwanCha <suhwan.cha@ezar.co.kr>
2024-04-03 12:23:38 +0900

    fix(android): check permission

--- android/src/main/kotlin/design/codeux/biometric_storage/BiometricStoragePlugin.kt
+++ android/src/main/kotlin/design/codeux/biometric_storage/BiometricStoragePlugin.kt
@@ -325,7 +325,7 @@ class BiometricStoragePlugin : FlutterPlugin, ActivityAware, MethodCallHandler {
         }
 
         val response = biometricManager.canAuthenticate(
-            BIOMETRIC_STRONG or BIOMETRIC_WEAK
+            BIOMETRIC_STRONG
         )
         return CanAuthenticateResponse.values().firstOrNull { it.code == response }
             ?: throw Exception(
